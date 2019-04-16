82a0f33cd4862bd42c0e97eacdfbaff375fc8b82
jungkooki <43691850+jungkooki@users.noreply.github.com>
2019-04-16 11:28:08 +0800

    lianxi

---
+++ agou/a.js
@@ -0,0 +1,8 @@
+
+function init() {
+  var divsign= document.getElementById('agou')
+  divsign.innerHTML= ''
+  divsign.setAttribute('style','width:100px;height:150px;margin-top:70px;margin-left:55px;background-color:blue;')
+  divsign.getAttribute('style')
+}
+window.onload = init;
